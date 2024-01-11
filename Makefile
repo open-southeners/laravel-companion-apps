a83e2b9db458ac93450b9af171b072782464bdae
Rubén Robles <me@d8vjork.com>
2024-01-11 14:12:59 +0100

    add facade support util by Laravel team

---
+++ Makefile
@@ -0,0 +1,5 @@
+facades:
+	vendor/bin/facade.php OpenSoutheners\\LaravelCompanionApps\\Support\\Facades\\Companion
+
+facades-lint:
+	vendor/bin/facade.php --lint OpenSoutheners\\LaravelCompanionApps\\Support\\Facades\\Companion
--- src/Support/Facades/Companion.php
+++ src/Support/Facades/Companion.php
@@ -5,6 +5,14 @@ namespace OpenSoutheners\LaravelCompanionApps\Support\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static array list()
+ * @method static array listByPlatform(\OpenSoutheners\LaravelCompanionApps\Platform $platform)
+ * @method static \OpenSoutheners\LaravelCompanionApps\CompanionApplication getByName(\OpenSoutheners\LaravelCompanionApps\Platform $platform, string $name)
+ * @method static \OpenSoutheners\LaravelCompanionApps\CompanionApplication android(string $name)
+ * @method static \OpenSoutheners\LaravelCompanionApps\CompanionApplication apple(string $name)
+ * @method static \OpenSoutheners\LaravelCompanionApps\CompanionApplication web(string $name)
+ * @method static string metaTags()
+ *
  * @see \OpenSoutheners\LaravelCompanionApps\Support\Companion
  */
 class Companion extends Facade
